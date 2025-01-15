59d03a7eb8aaa4c3361be2e6783ac9f0274d193d
niilliiz <niloo.vatan.nv@gmail.com>
2025-01-15 09:57:17 +0330

    feat: appwrite initial configs

---
+++ lib/appWrite/index.ts
@@ -0,0 +1,53 @@
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appWrite/config";
+import { cookies } from "next/headers";
+
+// session client -> linked to specific user session, letting users access to their data, perform actions they allow to
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  const session = (await cookies()).get("appwrite-session");
+  if (!session || !session.value) {
+    throw new Error("No Such session");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+
+    get database() {
+      return new Databases(client);
+    },
+  };
+}
+
+// admin client -> much more powerful, manage entire appwrite project, we gotta only use it on the server
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+
+    get database() {
+      return new Databases(client);
+    },
+
+    get storage() {
+      return new Storage(client);
+    },
+
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+}
