1c6b179dd33d025a89d4c0f71a2665838d82acef
niilliiz <niloo.vatan.nv@gmail.com>
2025-01-18 13:34:44 +0330

    feat: sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,11 +2,21 @@ import { ReactNode } from "react";
 import Sidebar from "@/components/Sidebar";
 import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
   return (
     <main className="flex h-screen">
-      <Sidebar />
+      <Sidebar {...currentUser} />
       <section className="flex h-ful flex-1 flex-col">
         <MobileNavigation />
         <Header />
