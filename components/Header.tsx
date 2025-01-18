769b37ea74d720afb26bcfd0ff40b0b42bea3dd9
niilliiz <niloo.vatan.nv@gmail.com>
2025-01-18 15:55:50 +0330

    feat: signout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,23 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
 export default function Header() {
+  async function handleSignOut() {
+    "use server";
+    try {
+      await signOutUser();
+    } catch (err) {
+      console.error(err);
+    }
+  }
   return (
     <header className="header">
       search
       <div className="header-wrapper">
-        file uploader
-        <form>
+        <FileUploader />
+        <form action={handleSignOut}>
           <Button type="submit" className="sign-out-button">
             <Image
               src="/assets/icons/logout.svg"
